83b5ad477ca0c2aa77278e5e45f900ccb06ad5d4
Frej Connolly <frej@connolly.se>
2014-12-15 16:30:53 +0100

    Fix wrong order of expected and actual in assert.

--- tests/test_swedish_to_english.py
+++ tests/test_swedish_to_english.py
@@ -26,37 +26,37 @@ class TestData(unittest.TestCase):
     def test_to_english_feel(self):
         actual = to_english(u'mår', self.dictionary)
         expected = {'feel'}
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_to_english_tree(self):
         actual = to_english(u'träd', self.dictionary)
         expected = {'tree'}
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_to_english_allihopa(self):
         actual = to_english('allihopa', self.dictionary)
         expected = {'all', 'one and all'}
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_to_english_swedish_word_returns_list_of_english_words(self):
         actual = to_english('hej', self.dictionary)
         expected = {'hello', 'hallo', 'hey', 'hi'}
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_on(self):
         actual = to_english(u'på', self.dictionary)
         expected = {'at etc', 'on', 'in', 'during', 'of', 'at', 'after', 'in'}
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_ett(self):
         actual = to_english(u'ett', self.dictionary)
         expected = {'a', 'an', 'one'}
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_dig(self):
         actual = to_english(u'dig', self.dictionary)
         expected = {'you'}
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_direct_translate(self):
         swedish_sentence = u'Hej på dig.'
@@ -65,66 +65,66 @@ class TestData(unittest.TestCase):
         expected = [{'hello', 'hallo', 'hey', 'hi'},
                    {'at etc', 'on', 'in', 'during', 'of', 'at', 'after', 'in'},
                     {'you'}]
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_get_most_probable_hi(self):
         fr = '<s>'
         to = ["hello", "hallo", "hi", "hey"]
         actual = get_most_probable(fr, to, self.bigrams)
         expected = 'hi'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_get_most_probable_a(self):
         fr = 'that'
         to = ["a", "one"]
         actual = get_most_probable(fr, to, self.bigrams)
         expected = 'a'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_get_most_probable_in(self):
         fr = 'active'
         to = ['on', 'and', 'in', 'yes', 'frej']
         actual = get_most_probable(fr, to, self.bigrams)
         expected = 'in'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_translate_hi_all(self):
         swedish_sentence = u'Hej allihopa.'
         actual = translate(swedish_sentence, self.dictionary, self.bigrams)
         expected = 'hi all'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_translate_how_are_you(self):
         swedish_sentence = u'Hur mår ni.'
         actual = translate(swedish_sentence, self.dictionary, self.bigrams)
         expected = 'how  '
         # XXX should fail due to semantical difference between SWE-ENG
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_translate_how_are_you_two(self):
         swedish_sentence = u'Är detta ett steg?'
         actual = translate_greedy(swedish_sentence, self.dictionary, self.bigrams)
         expected = 'is this one step'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_translate_how_are_you_tree(self):
         swedish_sentence = u'Är detta ett träd?'
         actual = translate_greedy(swedish_sentence, self.dictionary, self.bigrams)
         expected = 'is this one tree'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_xpath(self):
         actual = get_inflections(u'är', self.dictionary)
         expected = {'am/are/is'}
         # XXX should fail due to semantical difference between SWE-ENG
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_get_most_probable_a2(self):
         fr = 'this'
         to = ["a", "one"]
         actual = get_most_probable(fr, to, self.bigrams)
         expected = 'one'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_possible_words(self):
         last_word = "<s>"
@@ -132,22 +132,22 @@ class TestData(unittest.TestCase):
         words = english_words(swedish_sentence, self.dictionary)
         actual = possible_words(last_word, words[0], self.bigrams)
         expected = Counter({'is': 53121629, 'are': 26543233, 'am': 1596465})
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_translate_sum_is_this_a_tree(self):
         swedish_sentence = u'Är detta ett träd?'
         actual = translate_sum(swedish_sentence, self.dictionary, self.bigrams)
         expected = 'is this one tree'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_translate_min_is_this_a_tree(self):
         swedish_sentence = u'Är detta ett träd?'
         actual = translate_min(swedish_sentence, self.dictionary, self.bigrams)
         expected = 'is this a tree'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_translate_min_new(self):
         swedish_sentence = u'Jag hatar dig'
         actual = translate_min_new(swedish_sentence, self.dictionary, load_dictionary("folkets_en_sv_public.xml"), self.bigrams)
         expected = 'i hate you'
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
